47bc629fec0b814854531f646f16d899f38166cc
adamkkarl <adamkkarl@gmail.com>
2020-03-11 04:30:52 -0400

    added CarTracker.java

---
+++ p3/CarTracker.java
@@ -0,0 +1,19 @@
+// VIN:Make:Model:Price:Mileage:Color
+
+/*
+ * Adam Karl
+ * akk67
+ * CS 1501
+ * Project 3
+ */
+
+public class CarTracker {
+
+  private static void readFile() {
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
