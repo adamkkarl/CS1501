3a1a972e4cd9642e24ae2c0aa277ece512548f5f
adamkkarl <adamkkarl@gmail.com>
2020-03-23 14:46:59 -0400

    reads from cars.txt into array heap

--- p3/CarTracker.java
+++ p3/CarTracker.java
@@ -1,4 +1,6 @@
 // VIN:Make:Model:Price:Mileage:Color
+import java.util.*;
+import java.io.File;
 
 /*
  * Adam Karl
@@ -8,12 +10,55 @@
  */
 
 public class CarTracker {
+  private static class Heap {
+    List<Car> carsList;
 
-  private static void readFile() {
+    private void addCarFromFile(String info) {
+      Car car = new Car(info);
+    }
 
+    Heap() {
+      carsList = new ArrayList<Car>();
+    }
+
+    private static class Car {
+      String VIN, make, model, color;
+      int price, mileage;
+
+      Car (String info) {
+        String[] i = info.split(":");
+        VIN = i[0];
+        make = i[1];
+        model = i[2];
+        price = Integer.parseInt(i[3]);
+        mileage = Integer.parseInt(i[4]);
+        color = i[5];
+      }
+    }
+  }
+
+  private static void readFile(Heap cars) {
+  /*
+   * Given a Heap object, read the file "cars.txt" into the heap
+   */
+    try {
+      File f = new File("cars.txt");
+      Scanner sc = new Scanner(f);
+      while(sc.hasNext()) {
+        String line = sc.nextLine();
+        if (line.charAt(0) != '#') { //don't read in comment lines
+          cars.addCarFromFile(line);
+        }
+      }
+      sc.close();
+    } catch (Exception e) {
+      System.out.println("Issue reading cars.txt");
+    }
   }
 
   public static void main(String[] args) {
+    Heap cars = new Heap();
+    readFile(cars);
 
   }
 }
